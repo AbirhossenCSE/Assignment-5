c68bc06a12d9580a09219bd94f3892dd1779532c
Abir Hossen <abir.cse4@gmail.com>
2024-09-24 19:31:19 +0600

    donate

--- js/adddonation2.js
+++ js/adddonation2.js
@@ -1,7 +1,8 @@
-document.getElementById('btn-donate').addEventListener('click', function(event){
+// part 2
+document.getElementById('btn-donate-2').addEventListener('click', function(event){
     event.preventDefault();
 
-    const donateMoney = getInputFieldValueById('input-add-money');
+    const donateMoney = getInputFieldValueById('input-add-money-2');
     
 
     if (isNaN(donateMoney)) {
@@ -11,28 +12,30 @@ document.getElementById('btn-donate').addEventListener('click', function(event){
 
     if (donateMoney >= 0) {
         // donation balance
-        const donateBalance = getTextFieldValueById('donation-balance');
+        const donateBalance = getTextFieldValueById('donation-balance-2');
         // main account balance
         const accountBalance = getTextFieldValueById('account-balance');
         if (donateMoney > accountBalance) {
-            alert('No money')
+            alert('You do not have enoth balance');
+            return;
         }
 
         // add and cashout
-        const donationBalance = donateBalance + donateMoney;
+        const donateNewBalance = donateBalance + donateMoney;
         const accountNewBalance = accountBalance - donateMoney;
 
         // donation balance
-        document.getElementById('donation-balance').innerText = donationBalance;
+        document.getElementById('donation-balance-2').innerText = donateNewBalance;
         // main balance
         document.getElementById('account-balance').innerText = accountNewBalance;
 
 
 
         // add to transaction history
+        // 96500 Taka is Donated for famine-2024 at Feni, Bangladesh
         const p = document.createElement('p');
         p.classList.add('bg-yellow-300');
-        p.innerText = `${donateMoney} Taka is Donated for famine-2024 at Noakhali, Bangladesh`;
+        p.innerText = `${donateMoney} Taka is Donated for famine-2024 at Feni, Bangladesh`;
         // should be a common function
         document.getElementById('history-section').appendChild(p);
         
@@ -41,4 +44,3 @@ document.getElementById('btn-donate').addEventListener('click', function(event){
     }
     
 })
-
--- js/adddonation3.js
+++ js/adddonation3.js
@@ -1,7 +1,7 @@
-document.getElementById('btn-donate').addEventListener('click', function(event){
+document.getElementById('btn-donate-3').addEventListener('click', function(event){
     event.preventDefault();
 
-    const donateMoney = getInputFieldValueById('input-add-money');
+    const donateMoney = getInputFieldValueById('input-add-money-3');
     
 
     if (isNaN(donateMoney)) {
@@ -11,28 +11,30 @@ document.getElementById('btn-donate').addEventListener('click', function(event){
 
     if (donateMoney >= 0) {
         // donation balance
-        const donateBalance = getTextFieldValueById('donation-balance');
+        const donateBalance = getTextFieldValueById('donation-balance-3');
         // main account balance
         const accountBalance = getTextFieldValueById('account-balance');
         if (donateMoney > accountBalance) {
-            alert('No money')
+            alert('You do not have enoth balance');
+            return;
         }
 
         // add and cashout
-        const donationBalance = donateBalance + donateMoney;
+        const donateNewBalance = donateBalance + donateMoney;
         const accountNewBalance = accountBalance - donateMoney;
 
         // donation balance
-        document.getElementById('donation-balance').innerText = donationBalance;
+        document.getElementById('donation-balance-2').innerText = donateNewBalance;
         // main balance
         document.getElementById('account-balance').innerText = accountNewBalance;
 
 
 
         // add to transaction history
+        // 15500 Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh
         const p = document.createElement('p');
         p.classList.add('bg-yellow-300');
-        p.innerText = `${donateMoney} Taka is Donated for famine-2024 at Noakhali, Bangladesh`;
+        p.innerText = `${donateMoney} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh`;
         // should be a common function
         document.getElementById('history-section').appendChild(p);
         
@@ -41,4 +43,3 @@ document.getElementById('btn-donate').addEventListener('click', function(event){
     }
     
 })
-
